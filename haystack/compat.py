19027fadef1fe372a40cd4e1212652d8b123d246
Javier Buzzi <buzzi.javier@gmail.com>
2025-08-21 03:46:24 -0400

    .

--- haystack/compat.py
+++ haystack/compat.py
@@ -6,6 +6,8 @@ try:
 except PackageNotFoundError:
     elasticsearch_version = Version("0")
 
-HAS_ES7 = Version("7") > elasticsearch_version < Version("8")
-HAS_ES8 = Version("8") > elasticsearch_version < Version("9")
-HAS_ES9 = Version("9") > elasticsearch_version < Version("10")
+print("*"*20, elasticsearch_version)
+
+HAS_ES7 = Version("7") >= elasticsearch_version < Version("8")
+HAS_ES8 = Version("8") >= elasticsearch_version < Version("9")
+HAS_ES9 = Version("9") >= elasticsearch_version < Version("10")
